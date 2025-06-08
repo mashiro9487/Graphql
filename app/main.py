7985e1b37778f999c38f648879eed1bc64ad3ce8
mashiro9487 <joeisgod99@gmail.com>
2025-06-08 11:58:21 +0800

    Add success message for GraphQL schema loading

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
 from ariadne import make_executable_schema, gql
 from ariadne.asgi import GraphQL
+from fastapi import FastAPI
+
 from .resolvers import query, mutation
 
 # 讀取 GraphQL schema
@@ -9,6 +10,7 @@ with open("app/schema.graphql", "r") as f:
 
 # 組合 schema
 schema = make_executable_schema(type_defs, query, mutation)
+print("GraphQL schema loaded successfully.")
 
 # 啟用 FastAPI 應用
 app = FastAPI()
