2b31b4cb717cec358bb3a093561a188495467567
mashiro9487 <joeisgod99@gmail.com>
2025-06-14 15:45:53 +0800

    revise main

--- app/main.py
+++ app/main.py
@@ -1,32 +1,29 @@
 from fastapi import FastAPI
-from ariadne.asgi import GraphQL
-from ariadne import QueryType, gql, make_executable_schema
 from fastapi.middleware.cors import CORSMiddleware
+from ariadne import make_executable_schema, gql
+from ariadne.asgi import GraphQL
 
-# CORS 設定
+# 匯入 Query/Mutation Resolver
+from .resolvers import query, mutation  # 假設你有 app/resolvers.py
+
+# 讀取 schema.graphql
+with open("app/schema.graphql", "r") as f:
+    type_defs = gql(f.read())
+
+# 建立 Ariadne schema
+schema = make_executable_schema(type_defs, query, mutation)
+
+# 建立 FastAPI 應用
 app = FastAPI()
+
+# 加入 CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # 或 ["https://yourfrontend.com"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# GraphQL schema
-type_defs = gql("""
-    type Query {
-        hello: String!
-    }
-""")
-
-query = QueryType()
-
-@query.field("hello")
-def resolve_hello(_, info):
-    return "Hello from GraphQL!"
-
-schema = make_executable_schema(type_defs, query)
-
-# 掛載 GraphQL 到 /graphql 路徑
+# 掛載 /graphql endpoint
 app.mount("/graphql", GraphQL(schema, debug=True))
